e3266915c480856ecd8afb9cca5bbc20e3f30ed3
Emmanuel Jeremiah Perumal <emmanuel@multitier.co.za>
2021-07-02 11:42:08 +0200

    abstracted c# code into base class for login page

---
+++ MTSServiceDesk/MTSServiceDesk/Client/Pages/Authentication/LoginBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MTS.ServiceDesk.Client.Services;
+using MTS.ServiceDesk.Shared.Models;
+
+namespace MTS.ServiceDesk.Client.Pages.Authentication
+{
+    public class LoginBase : ComponentBase
+
+    {
+        [Inject] protected NavigationManager navigationManager { get; set; }
+            [Inject] protected CustomStateProvider authStateProvider { get; set; }
+       protected LoginRequest loginRequest { get; set; } = new LoginRequest();
+      protected  string error { get; set; }
+
+
+       protected async Task OnSubmit()
+        {
+            error = null;
+            try
+            {
+                await authStateProvider.Login(loginRequest);
+                navigationManager.NavigateTo("");
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+        }
+    }
+}
