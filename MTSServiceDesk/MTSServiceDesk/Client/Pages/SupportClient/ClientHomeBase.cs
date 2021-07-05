b9c65282e63e7a8e2a66bbc97be17df4f6f1dcd4
Emmanuel Jeremiah Perumal <emmanuel@multitier.co.za>
2021-07-05 16:01:13 +0200

    abstracted C# code from clienthome.razor file into new c# class file ClientHomeBase.cs

--- MTSServiceDesk/MTSServiceDesk/Client/Pages/SupportClient/ClientHomeBase.cs
+++ MTSServiceDesk/MTSServiceDesk/Client/Pages/SupportClient/ClientHomeBase.cs
@@ -1,19 +1,24 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Components;
+using MTS.ServiceDesk.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
-//namespace MTS.ServiceDesk.Client.Pages.SupportClient
-//{
-//    public class ClientHomeBase : ComponentBase
-//    {
-//        private SupportClientDetails[] clients;
+namespace MTS.ServiceDesk.Client.Pages.SupportClient
+{
+    public class ClientHomeBase : ComponentBase
+    {
+        protected SupportClientDetails[] clients;
+        [Inject] protected HttpClient httpClient { get; set; }
 
-//        protected override async Task OnInitializedAsync()
-//        {
-//            clients = await httpClient.GetFromJsonAsync<SupportClientDetails[]>("api/SupportClient/Get-All");
+        protected override async Task OnInitializedAsync()
+        {
+            clients = await httpClient.GetFromJsonAsync<SupportClientDetails[]>("api/SupportClient/Get-All");
 
-//            //return base.OnInitializedAsync();
-//        }
-//    }
-//}
+            //return base.OnInitializedAsync();
+        }
+    }
+}
