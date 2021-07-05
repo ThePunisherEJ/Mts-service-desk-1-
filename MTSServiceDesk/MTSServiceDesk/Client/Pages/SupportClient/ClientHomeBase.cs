8e0a4b1418e82ad7a6b68fa0bf126a8988469e27
Emmanuel Jeremiah Perumal <emmanuel@multitier.co.za>
2021-07-05 15:44:00 +0200

    Renamed testclient page to clienthome.razor , added buttons(newclient and Edit) to table on the client home page

---
+++ MTSServiceDesk/MTSServiceDesk/Client/Pages/SupportClient/ClientHomeBase.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace MTS.ServiceDesk.Client.Pages.SupportClient
+//{
+//    public class ClientHomeBase : ComponentBase
+//    {
+//        private SupportClientDetails[] clients;
+
+//        protected override async Task OnInitializedAsync()
+//        {
+//            clients = await httpClient.GetFromJsonAsync<SupportClientDetails[]>("api/SupportClient/Get-All");
+
+//            //return base.OnInitializedAsync();
+//        }
+//    }
+//}
