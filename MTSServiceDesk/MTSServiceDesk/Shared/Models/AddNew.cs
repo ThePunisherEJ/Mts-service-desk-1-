4c2ec79fc296b69561562643a9673806a1af65ce
Emmanuel Jeremiah Perumal <emmanuel@multitier.co.za>
2021-07-08 16:28:21 +0200

    Created files(AddNew.cs, NewClient.Razor) Added basic styling to newclient page used editforms and data validations on new client page as well as necessary C#

---
+++ MTSServiceDesk/MTSServiceDesk/Shared/Models/AddNew.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MTS.ServiceDesk.Shared.Models
+{
+   public class AddNew
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string DomainName { get; set; }
+
+
+    }
+}
