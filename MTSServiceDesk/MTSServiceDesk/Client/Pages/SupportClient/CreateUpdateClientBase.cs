8fb20e3c82af66a96aa16741cf1dac89b72a7960
Emmanuel Jeremiah Perumal <emmanuel@multitier.co.za>
2021-07-21 13:56:06 +0200

    Added Upload file element to createupdateclient page

--- MTSServiceDesk/MTSServiceDesk/Client/Pages/SupportClient/CreateUpdateClientBase.cs
+++ MTSServiceDesk/MTSServiceDesk/Client/Pages/SupportClient/CreateUpdateClientBase.cs
@@ -8,6 +8,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+using BlazorInputFile;
+using System.IO;
 
 namespace MTS.ServiceDesk.Client.Pages.SupportClient
 {
@@ -21,13 +24,34 @@ namespace MTS.ServiceDesk.Client.Pages.SupportClient
         [Parameter] public int Id { get; set; }
         protected SupportClientCreateUpdateRequest ClientRequest = new SupportClientCreateUpdateRequest();
         [CascadingParameter] public IModalService Modal { get; set; }
+
+
         #endregion
 
+       protected string status;
+       protected string imageDataUri;
+       protected async Task HandleSelection(IFileListEntry[] files)
+        {
+            var rawFile = files.FirstOrDefault();
+            if (rawFile != null)
+            {
+                // Load as an image file in memory
+                var format = "image/jpeg";
+                var imageFile = await rawFile.ToImageFileAsync(format, 640, 480);
+                var ms = new MemoryStream();
+                await imageFile.Data.CopyToAsync(ms);
 
+                // Make a data URL so we can display it
+                imageDataUri = $"data:{format};base64,{Convert.ToBase64String(ms.ToArray())}";
+
+                status = $"Finished loading {ms.Length} bytes from {imageFile.Name}";
+            }
+        }
         protected void Create()
         {
             Console.WriteLine(ClientRequest.Name);
         }
+        
 
         protected override async Task OnInitializedAsync()
         {
@@ -68,4 +92,8 @@ namespace MTS.ServiceDesk.Client.Pages.SupportClient
         }
     }
     
-}
+
+    
+    }
+
+
