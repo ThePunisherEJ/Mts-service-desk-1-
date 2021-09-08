4cc026b09a75c2f64c7df1453cb61b60005e3c2d
Magen Nayager <magenn@multitier.co.za>
2021-09-08 09:33:47 +0200

    added Ticket API Stub

---
+++ MTSServiceDesk/MTSServiceDesk/Server/Controllers/TicketController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTS.ServiceDesk.Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTS.ServiceDesk.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private ApplicationDBContext _applicationDBContext;
+
+
+        public TicketController(ApplicationDBContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+
+        }
+
+
+        //Get all tickets for Client
+        //Get open tickets for client
+        //Get Ticket by ID
+        //Get Comments for Ticket
+
+        //create Ticket
+        //update ticket
+        //changeticket status
+
+        // add comment
+
+
+    }
+}
