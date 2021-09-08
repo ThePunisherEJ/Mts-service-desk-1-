f61e1ad50c1b489659a5749016d6245a7423dc9e
Magen Nayager <magenn@multitier.co.za>
2021-09-08 09:13:31 +0200

    SQL added Ticket and Comment tables with FKs

---
+++ MTSServiceDesk.SQL/dbo/Tables/SupportTicket.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[SupportTicket]
+(
+	[Id] INT NOT NULL identity(1,1),
+	[DateCreated] datetime not null,
+	[CreatedBy] VARCHAR (150)     NOT NULL,
+	[ClientId] int not null,
+	[SystemId] int not null,
+	[Description] varchar(max) not null, 
+
+
+    CONSTRAINT [PK_SupportTicket] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_SupportTicket_SupportClient] FOREIGN KEY ([ClientId]) REFERENCES [SupportClient]([Id]), 
+    CONSTRAINT [FK_SupportTicket_Systems] FOREIGN KEY ([SystemId]) REFERENCES [Systems]([Id]),
+)
